2aabd3f8189858bf7ab47999839f5e135782dcb3
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-12-27 20:21:48 +0200

    made some changes...

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
                <View className="flex-1 flex-col gap-4 items-center pt-8 px-6">
                   <Text className="text-4xl font-extrabold text-[#6c63ff] ">TicketPay</Text>
                   
-                  <View className=" flex-row items-center bg-white rounded-xl w-full p-4  shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]">
+                  <View className=" flex-row items-center bg-white rounded-xl w-full p-4  shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
                      <Image className="mr-4" source={require("./assets/julian-wan-unsplash-1.png")} />
                      <View className="flex-col justify-between">
                         <Text className=" font-bold text-2xl">IGIRANEZA Josue</Text>
@@ -40,8 +40,9 @@ export default function App() {
 
                   </View>
 
-                  <View className=" w-full flex-col space-y-2 bg-[#f0f2f5] rounded-3xl py-4">
-                     <View className="flex-row justify-between items-center ">
+                  <View className=" w-full flex-col bg-[#f0f2f5] rounded-3xl py-4">
+                     
+                     <View className="flex-row justify-between items-center mb-3 ">
                         <Text className="text-2xl font-bold">Unpaid Tickets</Text>
                         <Feather name="arrow-right-circle" size={24} color="black" />
                      </View>
