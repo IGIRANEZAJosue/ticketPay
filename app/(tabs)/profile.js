0a10d2d77fc65512a0fd081ea5ba5f98dfd623c1
IGIRANEZA Josue <igiranezaj28@gmail.com>
2024-01-24 09:22:02 +0200

    Made small ui changes (profile page)

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -1,65 +1,59 @@
 import { router } from "expo-router";
 import React from "react";
-import { View, Text, Image,TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import tw from "twrnc";
-import { FontAwesome } from '@expo/vector-icons';
-import { useEffect,useState } from "react";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-
+import { FontAwesome, Feather } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const profile = () => {
-   const [profileInfo,setProfileInfo]=useState()
- 
-   useEffect(()=>{
-      const getData = async ()=>{
-      const value = await AsyncStorage.getItem("userInfo");
-      const parsedValue = JSON.parse(value);
-      if(parsedValue.user._id){
+   const [profileInfo, setProfileInfo] = useState();
 
-         setProfileInfo(parsedValue)}
-      else{
-         router.replace("/login");
+   useEffect(() => {
+      const getData = async () => {
+         const value = await AsyncStorage.getItem("userInfo");
+         const parsedValue = JSON.parse(value);
+         if (parsedValue.user._id) {
+            setProfileInfo(parsedValue);
+         } else {
+            router.replace("/login");
+         }
+      };
+      if (!profileInfo) {
+         getData();
       }
-   }
-   if(!profileInfo){
-   getData()}
-   },[])
+   }, []);
    const handleLogout = async () => {
-      
-           await AsyncStorage.removeItem('userInfo');
-          await AsyncStorage.removeItem("userToken");
-         //   setIsLoggedIn(true);
-         router.replace("/login");
-          
-       
+      await AsyncStorage.removeItem("userInfo");
+      await AsyncStorage.removeItem("userToken");
+      //   setIsLoggedIn(true);
+      router.replace("/login");
    };
    return (
-      <View style={tw`bg-white h-full px-8 `}>
-         <Text style={tw`text-3xl text-center mt-4 font-bold`}>
-            Profile
-         </Text>
+      <View style={tw`bg-white h-full px-8 items-center `}>
+         <Text style={tw`text-3xl text-center mt-4 font-bold`}>Profile</Text>
 
          <View style={tw` justify-center items-center`}>
-            <Image style={tw`h-[120px] w-[120px] mt-6`} source={require("../../assets/julian-wan-unsplash-1.png")} />
-            <View>
-               <Text style={tw`text-center`}>{profileInfo&&profileInfo.user.name}</Text>
-               <Text style={tw`text-center`}>{profileInfo&&profileInfo.user.email}</Text>
+            <Image
+               style={tw`h-[120px] w-[120px] mt-6`}
+               source={require("../../assets/julian-wan-unsplash-1.png")}
+            />
+            <View style={tw`my-4`}>
+               <Text style={tw`text-center text-lg font-semibold`}>
+                  {profileInfo && profileInfo.user.name}
+               </Text>
+               <Text style={tw`text-center `}>
+                  {profileInfo && profileInfo.user.email}
+               </Text>
             </View>
          </View>
          <TouchableOpacity
-               onPress={handleLogout}
-               style={tw`w-full justify-center items-center py-4 bg-[#6c63ff] rounded-lg my-4`}
-               className=" w-full justify-center items-center py-4 bg-[#6c63ff] rounded-lg my-4"
-            >
-               <Text
-                  style={tw`text-[16px] text-white font-bold`}
-                  className=" text-[16px] text-white font-bold"
-               >
-                  LOGOUT
-               </Text>
-            </TouchableOpacity>
-
+            onPress={handleLogout}
+            style={tw` bg-[#6c63ff]/90 flex-row items-center justify-center w-[150px] px-8 py-3 rounded-lg mt-8 `}
+         >
+            <Text style={tw`text-white font-bold`}>Logout</Text>
+            <Feather name="log-out" size={18} color="white" />
+         </TouchableOpacity>
       </View>
    );
 };
